433801f79d18d1b4d0a4eaaccb981fd76bde16a6
berabjesus <berabjesus@gmail.com>
2021-10-12 08:47:08 -0500

    add test for saving account

--- w3d1-js_class/test.js
+++ w3d1-js_class/test.js
@@ -2,7 +2,6 @@ describe("Account", function () {
   let account;
   before(function(){
       account = new Account(1234);
-      account.deposit(100);
 
   });
 
@@ -13,6 +12,7 @@ describe("Account", function () {
 
   it("deposites money to account",
   function () {
+      account.deposit(100);
       assert.equal(100, account.getBalance());
   });
 
@@ -40,12 +40,44 @@ describe("Account", function () {
 });
 
 
-describe("Account", function () {
+describe("Savings Account", function () {
   let account;
   before(function(){
-      account = new Account(1234);
+      account = new SavingsAccount(1235, 10);
       account.deposit(100);
+  });
 
+  it("must be a child of an Account class",
+  function () {
+      assert.equal(true, SavingsAccount.prototype instanceof Account);
+  });
+
+  
+  it("sets and gets interest",
+  function () {
+      account.setInterest(12);
+      assert.equal(12, account.getInterest());
+  });
+
+  it("adds interest amount to deposit",
+  function () {
+      account.addInterest();
+      assert.equal(112, account.getBalance());
+  });
+
+  it("returns correct string format",
+  function () {
+      let expect = "Account " + account.getNumber() + ": balance " + account.getBalance()+ " : interest rate " + account.getInterest();
+      assert.equal(expect, account.toString())
+  });
+
+  it("returns end of month statment",
+  function () {
+      let interestAdded = account.getBalance() * (account.getInterest() / 100);
+
+      let expect = "Interest added SavingAccount " + account.getNumber() + ": balance: " + account.getBalance() + " interest: " + interestAdded;
+
+      assert.equal(expect, account.endOfMonth())
   });
 
 });
