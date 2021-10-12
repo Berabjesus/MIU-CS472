03df648150bb845bfc5448a4cec23b582e4bebc9
berabjesus <berabjesus@gmail.com>
2021-10-12 08:32:42 -0500

    add tests for account

--- w3d1-js_class/test.js
+++ w3d1-js_class/test.js
@@ -31,4 +31,21 @@ describe("Account", function () {
       assert.throws(() => account.withdraw(101) , Error, "Insufficient funds");
   });
 
+  it("returns correct string format",
+  function () {
+      let expect = "Account " + account.getNumber() + ": balance " + account.getBalance()
+      assert.equal(expect, account.toString());
+  });
+
+});
+
+
+describe("Account", function () {
+  let account;
+  before(function(){
+      account = new Account(1234);
+      account.deposit(100);
+
+  });
+
 });
