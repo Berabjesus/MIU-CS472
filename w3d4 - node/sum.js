f0b7b85915b72059c063f3455dd91f8bfc39d478
berabjesus <berabjesus@gmail.com>
2021-10-15 01:42:22 -0500

    add math.js

--- w3d4 - node/sum.js
+++ w3d4 - node/sum.js
@@ -1,19 +1,18 @@
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
- });
+});
 
- const question = (collector) => {
-  readline.question("Enter a number ", input  => {
+const question = (collector) => {
+  readline.question("Enter a number ", input => {
     if (/^[0-9-]+$/.test(input)) collector += parseInt(input)
+
     if (input === "stop") {
       console.log(collector);
       readline.close();
     } else question(collector)
-   })
-  
- }
+  })
 
- question(0);
+}
 
-// console.log(/^[0-9]+$/.test("4"));
+question(0);
