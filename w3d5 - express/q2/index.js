35eda990a58e1145f70086f3d6ae1cb67af0266b
berabjesus <berabjesus@gmail.com>
2021-10-16 04:03:17 -0500

    add q3

--- w3d5 - express/q2/index.js
+++ w3d5 - express/q2/index.js
@@ -17,6 +17,5 @@ app.post('/result', (req, res) => {
     let name = req.body.name;
     let age = req.body.age;
     res.send(`Welcome ${name}, you are ${age} years old`);
-
 });
 app.listen(3000);
