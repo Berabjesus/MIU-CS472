f356c96dddc56ddc4da4f3a7e9ef577b26b5fc6d
berabjesus <berabjesus@gmail.com>
2021-10-18 02:33:50 -0500

    add w3d6 q2

---
+++ w3d6 - mvc/q2/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "view"));
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.render('form')
+});
+app.post('/result', (req, res) => {
+    let name = req.body.name;
+    let age = req.body.age;
+    console.log(name,age);
+    res.render('result', {name : name, age : age})
+});
+app.listen(3000);
